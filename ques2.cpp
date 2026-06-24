eff0831ad279cbe34e38f596bd8e1a50e0361777
Hardik-112006 <hardiktalwar2006@gmail.com>
2026-06-24 15:59:49 +0530

    Add files via upload

--- ques2.cpp
+++ ques2.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int reverse(int n){
-    int rem,ans=0;
-    if(n>5000){
-        return 0;
-    }
-    else{
+
+ int setBits(int n) {
+    
+        int count = 0;
+        
         while(n!=0){
-            rem = n % 10;
-            ans = ans * 10 + rem;
-            n = n / 10;
+            
+        if(n&1){
+            count++;
         }
+        n=n>>1;
+        }
+        return count;
     }
-    return ans;
-}
 
 int main(){
-    int num;
-    cin >> num;
+    int n;
+    cin >> n;
 
-    cout << reverse(num);
+    cout << setBits(n);
 }
