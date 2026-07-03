235ad543254e45f0393a17076c7ec96ba1717ca7
Hardik-112006 <hardiktalwar2006@gmail.com>
2026-07-03 15:26:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (47.93%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        int i;
+        string ans;
+
+        for(i=0;i<address.size();i++){
+            if(address[i] == '.'){
+                ans += "[.]";
+            }
+            else{
+                ans += address[i];
+            }
+        }
+        return ans;
+    }
+};
